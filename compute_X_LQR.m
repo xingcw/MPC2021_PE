a2374443c47bbc974edec796c9128f32515b3bff
xing chunwei <xingcw602@sina.com>
2021-05-21 17:46:25 +0200

    final version

--- compute_X_LQR.m
+++ compute_X_LQR.m
@@ -20,6 +20,49 @@ function [A_x, b_x] = compute_X_LQR(Q, R)
     b_x = [param.Xcons(:,2);-param.Xcons(:,1);param.Ucons(:,2);-param.Ucons(:,1)];
     % State constraint set
     X = Polyhedron(A_x,b_x);
-    [A_x,b_x] = mRPI(A_c,X,30);
+    
+    % hardcode, as only 30 steps for MPC
+    N = 30;
+    
+    X_list = cell(N, 1);
+    X_list{1} = X;
+    
+    index = 0;
+    for i=1:1:N
+        X_last = X_list{i};
+        X_list{i+1} = intersect(X_last,Polyhedron(X_last.A*A_c,X_last.b));
+        if (mldivide(X_list{i},X_list{i+1}).volume<0.00001)
+            index = i;
+            break;
+        end
+    end
+    
+    % plot last polyhedron
+    plot(X_list{index}, 'color', 'lightgreen');
+    hold on;
+    
+    % plot initial plot
+    % initial condition for ex7
+    T_init_1 = [-2.25, 1.75, 0.75]';
+    
+    scatter3(T_init_1(1), T_init_1(2), T_init_1(3), 'r', 'filled');
+    init_cond_text = sprintf('init cond1');
+    text(T_init_1(1)+0.1, T_init_1(2), T_init_1(3), init_cond_text);
+    
+    % initial condition for ex8
+    T_init_2 = [1.5, 2.75, -0.25]';
+    
+    scatter3(T_init_2(1), T_init_2(2), T_init_2(3), 'k', 'filled');
+    init_cond_text = sprintf('init cond2');
+    text(T_init_2(1)+0.1, T_init_2(2), T_init_2(3), init_cond_text);
+    
+    alpha 0.5;
+    title('maximal Invariant Set');
+    xlabel('T_VC');
+    ylabel('T_F1');
+    zlabel('T_F2');
+    
+    A_x = X_list{index}.A;
+    b_x = X_list{index}.b;
 
 end
--- run_simulations.m
+++ run_simulations.m
@@ -24,102 +24,103 @@ disp('Unconstraint optimal control');
 
 % ===================== task 5 ============================
 % Uncontrolled system
-% figure(1); set(gcf, 'WindowStyle' ,'normal');
+figure(1); set(gcf, 'WindowStyle' ,'normal');
 T0 = T_sp;
-% simulate_building(T0);
-
-Q = diag([4109479,2064982,2076632]);
-R = eye(3);
+simulate_building(T0);
 
 % ====================== task 7 ============================
 % Tuning of LQR on first initial condition T_init^(1)
 T0_1 = T_sp+[-2.25;1.75;0.75];
-% [Q,R] = heuristic_LQR_tuning(2500, T0_1, T_sp, scen1);
-% figure(3); set(gcf, 'WindowStyle' ,'docked');
-% simulate_building(T0_1, @controller_lqr, Q, R, scen1, 1);
+[Q,R] = heuristic_LQR_tuning(2500, T0_1, T_sp, scen1);
+figure(3); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_1, @controller_lqr, Q, R, scen1, 1);
+pause;
 
 % ====================== task 8 ============================
 % Tuning of LQR on second initial condition T_init^(2)
 T0_2 = T_sp+[1.5;2.75;-0.25];
-% [Q_2,R_2] = heuristic_LQR_tuning(2500, T0_2, T_sp, scen1);
-% figure(4); set(gcf, 'WindowStyle' ,'docked');
-% simulate_building(T0_2, @controller_lqr, Q, R, scen1, 1);
-% pause;
+figure(4); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_2, @controller_lqr, Q, R, scen1, 1);
+pause;
 
 
 %% From LQR to MPC
 disp('First MPC'); 
-% Q = diag([4109479,2064982,2076632]);
-% R = eye(3);
-% Q_2 = diag([5792829, 777500, 472315]);
-% R_2 = eye(3);
-% [A_x,b_x] = compute_X_LQR(Q,R);
+Q = diag([4109479,2064982,2076632]);
+R = eye(3);
+figure(42); set(gcf, 'WindowStyle' ,'docked');
+[A_x,b_x] = compute_X_LQR(Q,R);
+pause;
+
 % ====================== task 11 ============================
-% figure(5); set(gcf, 'WindowStyle' ,'docked');
-% [~, ~, J_11] = simulate_building(T0_1, @controller_mpc_1, Q, R, scen1, 1);
-% figure(6); set(gcf, 'WindowStyle' ,'docked');
-% [~, ~, J_12] = simulate_building(T0_2, @controller_mpc_1, Q, R, scen1, 1);
-% pause;
+figure(5); set(gcf, 'WindowStyle' ,'docked');
+[~, ~, J_11] = simulate_building(T0_1, @controller_mpc_1, Q, R, scen1, 1);
+figure(6); set(gcf, 'WindowStyle' ,'docked');
+[~, ~, J_12] = simulate_building(T0_2, @controller_mpc_1, Q, R, scen1, 1);
+pause;
 
 
 %% MPC with guarantees
-% disp('MPC with guarantees');
+disp('MPC with guarantees');
 % ===================== task 13 =============================
-% figure(7); set(gcf, 'WindowStyle' ,'docked');
-% [~, ~, J_21] = simulate_building(T0_1, @controller_mpc_2, Q, R, scen1, 1);
-% figure(8); set(gcf, 'WindowStyle' ,'docked');
-% [~, ~, J_22] = simulate_building(T0_2, @controller_mpc_2, Q, R, scen1, 1);
+figure(7); set(gcf, 'WindowStyle' ,'docked');
+[~, ~, J_21] = simulate_building(T0_1, @controller_mpc_2, Q, R, scen1, 1);
+figure(8); set(gcf, 'WindowStyle' ,'docked');
+[~, ~, J_22] = simulate_building(T0_2, @controller_mpc_2, Q, R, scen1, 1);
+pause;
 
 % ===================== task 14 =============================
-% figure(9); set(gcf, 'WindowStyle' ,'docked');
-% [~, ~, J_31] = simulate_building(T0_1, @controller_mpc_3, Q, R, scen1, 1);
-% figure(10); set(gcf, 'WindowStyle' ,'docked');
-% [~, ~, J_32] = simulate_building(T0_2, @controller_mpc_3, Q, R, scen1, 1);
-% pause;
+figure(9); set(gcf, 'WindowStyle' ,'docked');
+[~, ~, J_31] = simulate_building(T0_1, @controller_mpc_3, Q, R, scen1, 1);
+figure(10); set(gcf, 'WindowStyle' ,'docked');
+[~, ~, J_32] = simulate_building(T0_2, @controller_mpc_3, Q, R, scen1, 1);
+pause;
 
 
 %% Soft-constrained MPC
-% disp('Soft-constrained MPC');
+disp('Soft-constrained MPC');
 % ===================== task 17 =============================
-% figure(11); set(gcf, 'WindowStyle' ,'docked');
-% simulate_building(T0_1, @controller_mpc_3, Q, R, scen2, 1);
+figure(11); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_1, @controller_mpc_3, Q, R, scen2, 1);
+pause;
 
 % ===================== task 18 =============================
-% figure(12); set(gcf, 'WindowStyle' ,'docked');
-% simulate_building(T0_1, @controller_mpc_4, Q, R, scen2, 1);
+figure(12); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_1, @controller_mpc_4, Q, R, scen2, 1);
+pause;
 
 % ===================== task 19 =============================
-% figure(13); set(gcf, 'WindowStyle' ,'docked');
-% simulate_building(T0_1, @controller_mpc_3, Q, R, scen1, 1);
-% figure(14); set(gcf, 'WindowStyle' ,'docked');
-% simulate_building(T0_1, @controller_mpc_4, Q, R, scen1, 1);
-%
+figure(13); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_1, @controller_mpc_3, Q, R, scen1, 1);
+figure(14); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_1, @controller_mpc_4, Q, R, scen1, 1);
+pause;
+
 % ===================== task 20 =============================
-% N=30;
-% d = zeros(3,scen2.Nbar+N);
-% d(1,37:51) = -0.15*ones(1,15);
-% d(2,37:44) = 0.8*ones(1,8);
-% d(3,42:45) = -0.4*ones(1,4);
-% d(3,45:50) = 0.7*ones(1,6);
-% % d(3,48:50) = 1*ones(1,3);
-% figure(15); set(gcf, 'WindowStyle' ,'docked');
-% simulate_building(T0_1, @controller_mpc_5, Q, R, scen2, 1, N, d);
-%pause;
+N=30;
+d = zeros(3,scen2.Nbar+N);
+d(1,37:51) = -0.15*ones(1,15);
+d(2,37:44) = 0.8*ones(1,8);
+d(3,42:45) = -0.4*ones(1,4);
+d(3,45:50) = 0.7*ones(1,6);
+figure(15); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_1, @controller_mpc_5, Q, R, scen2, 1, N, d);
+pause;
 
 
 %% Offset-free MPC
 disp('Offset-free MPC');
 % ===================== task 23 =============================
-% figure(16); set(gcf, 'WindowStyle' ,'docked');
-% simulate_building(T0_1,@controller_mpc_6,Q,R,scen3,1);
-% simulate_building(T0_1,@controller_mpc_3,Q,R,scen3,1);
-% pause;
-% 
-% 
+figure(16); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_1,@controller_mpc_6,Q,R,scen3,1);
+figure(17); set(gcf, 'WindowStyle' ,'docked');
+simulate_building(T0_1,@controller_mpc_3,Q,R,scen3,1);
+pause;
+
 %% Comparison using forces
 disp('MPC Implementation with FORCES Pro');
 % ===================== task 24 =============================
-% [~, ~, ~, t_sim_forces] = simulate_building(T0_2, @controller_mpc_1_forces, Q, R, scen1);
-% [~, ~, ~, t_sim] = simulate_building(T0_2, @controller_mpc_1, Q, R, scen1);
-% fprintf("FORCES MPC: %f s", t_sim_forces);
-% fprintf("NORMAL MPC: %f s", t_sim);
+[~, ~, ~, t_sim_forces] = simulate_building(T0_2, @controller_mpc_1_forces, Q, R, scen1);
+[~, ~, ~, t_sim] = simulate_building(T0_2, @controller_mpc_1, Q, R, scen1);
+fprintf("FORCES MPC: %f s", t_sim_forces);
+fprintf("NORMAL MPC: %f s", t_sim);
